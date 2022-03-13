10d32d0c4b6c779687d3344531ba445ecf97a440
Mle225 <Mle225@csu.fullerton.edu>
2022-03-13 13:47:14 +0700

    add support for mm:ss format input and video download format in mp4 instead of blob

--- pages/player.js
+++ pages/player.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-// import '../styles.css';
 
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 const ffmpeg = createFFmpeg({
@@ -16,7 +15,6 @@ export default function Player() {
     const [startTime, setStartTime] = useState(0.0);
     const [endTime, setEndTime] = useState (1.0);
     const [videoURL, setVidURL] = useState('');
-    // const [readyURL, setReadyURL] = useState(false);
     const [preview, setPreview] = useState();
 
     const load = async () => {
@@ -30,8 +28,24 @@ export default function Player() {
         load();
     }, [])
 
+    const getDuration = () => {    
+        if (endTime - startTime !== NaN) {
+            return String(endTime - startTime)
+        }
+        
+        const startSeconds = parseInt(startTime.slice(-2))
+        const startMinutes = parseInt(startTime.slice(0,3))
+        const startSum = (startMinutes * 60) + startSeconds
+
+        const endSeconds = parseInt(endTime.slice(-2))
+        const endMinutes = parseInt(endTime.slice(0,3))
+        const endSum = (endMinutes * 60) + endSeconds
+
+        return String(endSum - startSum)
+    }
+
     const trimVid = async () => {
-        const duration = String(endTime - startTime)
+        const duration = getDuration()
 
         // if URL is loaded, load from URL
         if (videoURL) {
@@ -50,7 +64,7 @@ export default function Player() {
         const data = ffmpeg.FS('readFile', 'out.mp4');
 
         // Create a URL
-        const url = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
+        const url = URL.createObjectURL(new Blob([data.buffer], { type: 'video\/mp4' }));
         setVid(url)
         console.log(url)
     }
@@ -78,7 +92,7 @@ export default function Player() {
                 <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
                 <input type="text" placeholder='Video URL here' onChange={(e) => {setVidURL(e.target.value)}} />
                 <button onClick= {() => loadVideo()}>
-                    Load Video 
+                    Load Video From URL
                 </button>
                 
                 <div>
@@ -97,11 +111,11 @@ export default function Player() {
                         src = {URL.createObjectURL(video)}>
                     </video>}
                 </div>
-
+            </div>
                 <div>
                     Input Start Time: 
                     <input type='text' placeholder='Start' onChange={(e) => {
-                        console.log(e.target.value)
+                        e.preventDefault();
                         setStartTime(e.target.value)
                     }} />
                 </div>
@@ -109,11 +123,11 @@ export default function Player() {
                 <div>
                     Input End Time:
                     <input type='text' placeholder='End' onChange={(e) => {
-                        console.log(e.target.value)
+                        e.preventDefault();
                         setEndTime(e.target.value)
                     }} />
                 </div>
-            </div>
+           
             
 
             <h3> Result </h3>
@@ -124,7 +138,7 @@ export default function Player() {
                 controls
                 src={vid} 
                 width="50%" 
-                type = "video/mp4"
+                type = "video\/mp4"
             />}
 
             <style jsx>{
