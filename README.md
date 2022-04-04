67eaf64df8c647f82cbc4904ba1499721dea839e
Farnam0 <87348664+Farnam0@users.noreply.github.com>
2022-04-04 11:49:28 -0700

    Improve UI responsiveness
    UI structure should be the same as specified in mockup document
    
    UI elements should reposition themselves according to screen size 
    
    Minimum width for Video Players should be 500px

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+## Instructions for running TruFans Video Clipper V 1.0
+1. Clone the repository into your local machine
+2.  After cloning, cd into the directory of the project
+3.  Make sure you have NPM and Node installed
+4.  Run npm install to install package dependencies
+5.  npm run dev to run the website in Localhost:3000
+
+--- 
+### Usage
+* You can upload videos locally or load videos from an external URL
+* Currently, only HTTP hosted videos can be used, as FFMPEG needs to be recompiled (TODO in v2)
+* For a list of test videos to use, please check out https://gist.github.com/jsturgis/3b19447b304616f18657
+* The input boxes for start/ end time take S.S or MM:SS format (e.g. 5.0 for 5 secs or 01:22 for 1 min 22 secs etc)
+* If you click trim and the video still does not show, please wait a bit
+* You can view the progress of the video processing if you Inspect and view the Browser Console
+
+---
+### Please note the following:
+* Videos will be outputted as mp4 (custom formatting in v2)
+* Videos will be processed in your local machine, and deleted when you quit the web app
+* If you run into error 4094, delete the .next folder in the root directory of the project and try again
--- pages/index.js
+++ pages/index.js
@@ -1,159 +1,116 @@
-import Link from 'next/link'
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+import trimVideo from '../components/TrimVideo';
+import loadVideo from '../components/LoadVideo';
+import { createFFmpeg } from '@ffmpeg/ffmpeg';
 
-export default function Home() {
-  return (
-    <div className="container">
-      <main>
-        <h1 className="title">
-          Go to <Link href="/player">Player</Link>
-        </h1>
-      </main>
+const ffmpeg = createFFmpeg({
+    corePath: "http://localhost:3000/ffmpeg-core.js",
+    // Use public address
+    log: true,
+  });
+  
+export default function Player() {
+    const [ready, setReady] = useState(false);
+    const [video, setVideo] = useState();
+    const [vid, setVid] = useState();
+    const [startTime, setStartTime] = useState(0.0);
+    const [endTime, setEndTime] = useState (1.0);
+    const [videoURL, setVidURL] = useState('');
+    const [urlPreviewVideo, setPreview] = useState();
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+    const load = async () => {
+        if (!ffmpeg.isLoaded()) {
+            await ffmpeg.load();
         }
+        setReady(true);
+    }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+    useEffect(() => {
+        load();
+    }, [])
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+    return ready ? 
+    (
+      <div className='vh-100 bg-light'>
+        <div className='container pt-5'>
+          <h1 className='text-center'>TruFans Video Clipper 2.0</h1>
+                <div className='form-group'>
+                  <label for="getFile">Pick Video Locally</label>
+                  <input type="file" class="form-control" id="getFile" onChange={(e) => setVideo(e.target.files?.item(0))} />
+                </div>
 
-        footer img {
-          margin-left: 0.5rem;
-        }
+                <div className='form-group pt-2 pb-2'>
+                  <label for="urlFile">Get Video From the Web</label>
+                  <input type="text" class="form-control" id="urlFile" placeholder='Video URL here' onChange={(e) => {setVidURL(e.target.value)}} />
+                </div>
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+                <button className='btn btn-primary' onClick= {() => loadVideo(videoURL, ffmpeg, setPreview)}>
+                      Load Video From URL
+                  </button>
+                
+              <div className='row'>
+              <div className='col text-center'>
+                {/* Preview player if user chooses to load video with url */}
+                <div className='pt-2 pb-2'>
+                    { urlPreviewVideo && <video
+                        controls 
+                        width="500"
+                        src= {urlPreviewVideo}
+                    ></video>
+                    }
+                </div>
+                
+                {/* Preview player if user chooses to load video from local system */}
+                <div className='pt-2 pb-2'>
+                    { video && <video
+                        controls
+                        width="500"
+                        src = {URL.createObjectURL(video)}>
+                    </video>}
+                </div>
+                </div>
+                </div>
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+            <div className='d-flex flex-column'>
+              <div class="input-group input-group-sm mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="inputGroup-sizing-sm">Input Start Time</span>
+                </div>
+                <input type="number" placeholder='Start' onChange={(e) => {
+                          e.preventDefault();
+                          setStartTime(e.target.value)
+                      }} />
+              </div>
+              <div class="input-group input-group-sm mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="inputGroup-sizing-sm">Input End Time</span>
+                </div>
+                <input type="number" placeholder='End' onChange={(e) => {
+                          e.preventDefault();
+                          setEndTime(e.target.value)
+                      }} />
+              </div>
+            </div>
+              <div className='row'>
+                <div className='col text-center'>
+                    <button className='btn btn-primary' onClick={() => trimVideo(endTime, startTime, videoURL, ffmpeg, video, setVid)}>Trim Video</button>
+                  </div>
+              </div>
+              <div className='row pt-5'>
+                <div className='col text-center'>
+              {/* Preview player for trimmed video */}
+              { vid && <video 
+                        controls
+                        src={vid} 
+                        width="50%" 
+                        type = "video\/mp4"
+                    />}
+                    </div>
+                    </div>
+        </div>
+      </div>
+    ) : (
+        <p> Loading . . . </p>
+    );
 }
