38840c60ddcc54e7fbfec8fcc8f3d4ead7470af2
Farnam0 <87348664+Farnam0@users.noreply.github.com>
2022-04-15 10:36:23 -0700

    C4G1-41 React player
    Get max duration from React player
    set slider max to the new duration

--- components/TimeLine.js
+++ components/TimeLine.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Slider from '@mui/material/Slider';
 import moment from 'moment';
 
-const Timeline = ({setStart, setEnd, startTime1, endTime1}) => {
-  const endLabel = moment().startOf('day').seconds(50).format('mm:ss');
-
+const Timeline = ({setStart, setEnd, startTime1, endTime1, maxValue}) => {
+  const endLabel = moment().startOf('day').seconds(maxValue).format('mm:ss');
+  
   const handleChange = (event, newValue) => {
     setStart(newValue[0]);
     setEnd(newValue[1]);
@@ -14,7 +14,7 @@ const Timeline = ({setStart, setEnd, startTime1, endTime1}) => {
       <div className="row">
         <Slider
           min={0}
-          max={50}
+          max={maxValue}
           value={[startTime1, endTime1]}
           onChange={handleChange}
           valueLabelDisplay="auto"
