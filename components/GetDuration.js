38189935025514636d8d39292d8e5ec9c92233da
raaamonnn <ograaamonnn@gmail.com>
2022-04-19 17:08:52 -0700

    [C4G1-46] - Cleaned up Code

--- components/GetDuration.js
+++ components/GetDuration.js
@@ -1,21 +1,20 @@
+const getDuration = (endTime, startTime) => {
+  // If the durations are already in 0.0 format, return the difference
+  if (endTime - startTime !== NaN) {
+    return String(endTime - startTime);
+  }
 
-const getDuration = (endTime, startTime) => {    
-    // If the durations are already in 0.0 format, return the difference
-    if (endTime - startTime !== NaN) {
-        return String(endTime - startTime)
-    }
+  // Convert start time in MM:SS format into S.S format
+  const startSeconds = parseInt(startTime.slice(-2));
+  const startMinutes = parseInt(startTime.slice(0, 3));
+  const startSum = startMinutes * 60 + startSeconds;
 
-    // Convert start time in MM:SS format into S.S format
-    const startSeconds = parseInt(startTime.slice(-2))
-    const startMinutes = parseInt(startTime.slice(0,3))
-    const startSum = (startMinutes * 60) + startSeconds
+  // Convert end time in MM:SS format into S.S format
+  const endSeconds = parseInt(endTime.slice(-2));
+  const endMinutes = parseInt(endTime.slice(0, 3));
+  const endSum = endMinutes * 60 + endSeconds;
 
-    // Convert end time in MM:SS format into S.S format
-    const endSeconds = parseInt(endTime.slice(-2))
-    const endMinutes = parseInt(endTime.slice(0,3))
-    const endSum = (endMinutes * 60) + endSeconds
-
-    return String(endSum - startSum)
-}
+  return String(endSum - startSum);
+};
 
 export default getDuration;
--- components/TimeLine.js
+++ components/TimeLine.js
@@ -1,19 +1,18 @@
+import React from "react";
+import Slider from "@mui/material/Slider";
+import moment from "moment";
 
-import React from 'react';
-import Slider from '@mui/material/Slider';
-import moment from 'moment';
+const Timeline = ({ setStart, setEnd, startTime1, endTime1, maxValue }) => {
+  const endLabel = moment().startOf("day").seconds(maxValue).format("mm:ss");
 
-const Timeline = ({setStart, setEnd, startTime1, endTime1, maxValue}) => {
-  const endLabel = moment().startOf('day').seconds(maxValue).format('mm:ss');
-  
   const handleChange = (event, newValue) => {
     event.preventDefault();
     setStart(newValue[0]);
     setEnd(newValue[1]);
-  }
-  
+  };
+
   return (
-    <div className="timeline col w-75 pb-3" style={{ paddingLeft: '25%' }}>
+    <div className="timeline col w-75 pb-3" style={{ paddingLeft: "25%" }}>
       <div className="row">
         <Slider
           min={0}
@@ -22,10 +21,10 @@ const Timeline = ({setStart, setEnd, startTime1, endTime1, maxValue}) => {
           onChange={handleChange}
           valueLabelDisplay="auto"
           valueLabelFormat={(val) => {
-            return moment().startOf('day').seconds(val).format('mm:ss');
+            return moment().startOf("day").seconds(val).format("mm:ss");
           }}
           step={0.1}
-          color={'primary'}
+          color={"primary"}
           aria-labelledby="range-slider"
         />
       </div>
@@ -37,6 +36,6 @@ const Timeline = ({setStart, setEnd, startTime1, endTime1, maxValue}) => {
       </div>
     </div>
   );
-}
+};
 
 export default Timeline;
